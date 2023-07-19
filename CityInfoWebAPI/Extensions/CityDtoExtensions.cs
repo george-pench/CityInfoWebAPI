40aaa3a76564c39a048fd04b4d1663ba00a84fa3
george-pench <penchev.georgii@gmail.com>
2023-07-20 00:04:12 +0300

    - Introduced 'CityDtoExtensions' class with a static 'AsDto' extension method. - The 'AsDto' method converts a 'City' object to a 'CityDto' object, mapping properties between the two classes.

---
+++ CityInfoWebAPI/Extensions/CityDtoExtensions.cs
@@ -0,0 +1,21 @@
+﻿using CityInfoWebAPI.Dtos;
+using CityInfoWebAPI.Models;
+
+namespace CityInfoWebAPI.Extensions
+{
+    public static class CityDtoExtensions
+    {
+        public static CityDto AsDto(this City city) 
+        {
+            return new CityDto
+            {
+                Id = city.Id,
+                Name = city.Name,
+                Country = city.Country,
+                Population = city.Population,
+                Timezone = city.Timezone,
+                CreatedDate = city.CreatedDate
+            };
+        }
+    }
+}
