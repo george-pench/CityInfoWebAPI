12f60b8ad7f2d7812fd9a3e33f5aa9006031fa3d
george-pench <penchev.georgii@gmail.com>
2023-07-19 23:41:31 +0300

    Add data validation attributes to City and CreateCityDto/UpdateCityDto.cs records
    - Applied [Required] attribute to the 'Name' and 'Country' properties in the 'City' record to enforce mandatory values.
    - Introduced [Required] and [Range] attributes to the 'Name', 'Country', and 'Population' properties in the 'CreateCityDto/UpdateCityDto' records for data validation.

---
+++ CityInfoWebAPI/Dtos/CityDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CityInfoWebAPI.Dtos
+{
+    public record CityDto
+    {
+        public Guid Id { get; init; }
+        public string Name { get; init; }
+        public string Country { get; init; }
+        public double Population { get; init; }
+        public string Timezone { get; init; }
+        public DateTimeOffset CreatedDate { get; init; }
+    }
+}
---
+++ CityInfoWebAPI/Dtos/UpdateCityDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CityInfoWebAPI.Dtos
+{
+    public record UpdateCityDto
+    {
+        [Required]
+        public string Name { get; init; }
+
+        [Required]
+        public string Country { get; init; }
+
+        [Required]
+        [Range(0, 100)]
+        public double Population { get; init; }
+
+        public string Timezone { get; init; }
+    }
+}
