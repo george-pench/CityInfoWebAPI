d13bb84c1856add3782fdb2c88a448f5c84627bc
george-pench <penchev.georgii@gmail.com>
2023-07-23 22:52:55 +0300

    Added a new class named `MongoDbConfigurations` in the `CityInfoWebAPI.Configurations` namespace. This class represents the configuration settings for connecting to the MongoDB database.
    Added a new class named `MongoDbCitiesRepository` in the `CityInfoWebAPI.Repositories` namespace. This class implements the `ICitiesRepository` interface and provides the implementation for interacting with the MongoDB database to perform CRUD operations on City entities.

---
+++ CityInfoWebAPI/Configurations/MongoDbConfigurations.cs
@@ -0,0 +1,17 @@
+﻿namespace CityInfoWebAPI.Configurations
+{
+    public class MongoDbConfigurations
+    {
+        public string Host { get; set; }
+
+        public int Port { get; set; }
+
+        public string ConnectionString
+        {
+            get 
+            {
+                return $"mongodb://{this.Host}:{this.Port}";
+            }
+        }
+    }
+}
---
+++ CityInfoWebAPI/Repositories/MongoDbCitiesRepository.cs
@@ -0,0 +1,45 @@
+﻿using CityInfoWebAPI.Models;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+
+namespace CityInfoWebAPI.Repositories
+{
+    public class MongoDbCitiesRepository : ICitiesRepository
+    {
+        private const string databaseName = "CityInfo";
+        private const string collectionName = "cities";
+
+        private readonly IMongoCollection<City> citiesCollection;
+        private readonly FilterDefinitionBuilder<City> filterDefinitionBuilder = Builders<City>.Filter;
+
+        public MongoDbCitiesRepository(IMongoClient mongoClient)
+        {
+            IMongoDatabase database = mongoClient.GetDatabase(databaseName);
+            citiesCollection = database.GetCollection<City>(collectionName);
+        }
+
+        public void CreateCity(City city) => this.citiesCollection.InsertOne(city);
+
+        public void DeleteCity(Guid id)
+        {
+            var filter = this.filterDefinitionBuilder.Eq(city => city.Id, id);
+            this.citiesCollection.DeleteOne(filter);
+        }
+
+        public IEnumerable<City> GetCities() => this.citiesCollection.Find(new BsonDocument()).ToList();
+
+        public City GetCity(Guid id)
+        {
+            var filter = this.filterDefinitionBuilder.Eq(city => city.Id, id);
+            return this.citiesCollection.Find(filter).SingleOrDefault();
+        }
+
+        public void UpdateCity(City city)
+        {
+            var filter = this.filterDefinitionBuilder.Eq(existingCity => existingCity.Id, city.Id);
+            this.citiesCollection.ReplaceOne(filter, city);
+        }
+    }
+}
