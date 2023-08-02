dd50501d49f015c8f698808cc1d6058241e494e3
george-pench <penchev.georgii@gmail.com>
2023-08-02 23:44:51 +0300

    - Implemented 'GetCityAsync_WithUnexistingCity_ReturnsNotFound' to test handling of non-existing city retrieval, ensuring a NotFound result is returned. - Created 'GetCityAsync_WithExistingCity_ReturnsExpectedCity' to verify that an existing city is correctly returned from the repository. - Added 'GetCitiesAsync_WithExistingCities_ReturnsAllCities' to validate the retrieval of all cities from the repository. - Implemented 'CreateCityAsync_WithCityToCreate_ReturnsCreatedCity' to test the creation of a new city, ensuring the returned city matches the input data.

---
+++ CityInfo.UnitTests/CitiesControllerTests.cs
@@ -0,0 +1,116 @@
+﻿using CityInfoWebAPI.Controllers;
+using CityInfoWebAPI.Dtos;
+using CityInfoWebAPI.Models;
+using CityInfoWebAPI.Repositories;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CityInfo.UnitTests
+{
+    public class CitiesControllerTests
+    {
+        private readonly Mock<ICitiesRepository> repositoryStub = new();
+        private readonly Random rand = new Random();
+
+        [Fact]
+        public async Task GetCityAsync_WithUnexistingCity_ReturnsNotFound()
+        {
+            // Arrange           
+            this.repositoryStub.Setup(repo => repo
+                .GetCityAsync(It.IsAny<Guid>()))
+                .ReturnsAsync((City)null);
+
+            var controller = new CitiesController(this.repositoryStub.Object);
+
+            // Act
+            var result = await controller.GetCityAsync(Guid.NewGuid());
+
+            // Assert
+            //Assert.IsType<NotFoundResult>(result.Result);
+            result.Result.Should().BeOfType<NotFoundResult>();
+        }
+
+        [Fact]
+        public async Task GetCityAsync_WithExistingCity_ReturnsExpectedCity()
+        {
+            // Arrange
+            var expectedCity = this.CreateRandomCity();
+
+            this.repositoryStub.Setup(repo => repo
+                .GetCityAsync(It.IsAny<Guid>()))
+                .ReturnsAsync(expectedCity);
+
+            var controller = new CitiesController(this.repositoryStub.Object);
+
+            // Act
+            var result = await controller.GetCityAsync(Guid.NewGuid());
+
+            // Assert
+            result.Value
+                .Should()
+                .BeEquivalentTo(expectedCity, options => options.ComparingByMembers<City>());
+        }
+
+        [Fact]
+        public async Task GetCitiesAsync_WithExistingCities_ReturnsAllCities()
+        {
+            // Arrange
+            var expectedCities = new[] { this.CreateRandomCity(), this.CreateRandomCity(), this.CreateRandomCity() };
+
+            this.repositoryStub.Setup(repo => repo.GetCitiesAsync())
+                .ReturnsAsync(expectedCities);
+
+            var controller = new CitiesController(this.repositoryStub.Object);
+
+            // Act
+            var actualCities = await controller.GetCitiesAsync();
+
+            // Assert
+            actualCities
+                .Should()
+                .BeEquivalentTo(expectedCities, options => options.ComparingByMembers<City>());
+        }
+
+        [Fact]
+        public async Task CreateCityAsync_WithCityToCreate_ReturnsCreatedCity()
+        {
+            // Arrange
+            var cityToCreate = new CreateCityDto()
+            {
+                Name = Guid.NewGuid().ToString(),
+                Country = Guid.NewGuid().ToString(),
+                Population = rand.Next(100),
+                Timezone = Guid.NewGuid().ToString()
+            };
+            
+            var controller = new CitiesController(this.repositoryStub.Object);
+
+            // Act
+            var result = await controller.CreateCityAsync(cityToCreate);
+
+            // Assert
+            var createdCity = (result.Result as CreatedAtActionResult).Value as CityDto;
+
+            cityToCreate
+                .Should()
+                .BeEquivalentTo(createdCity, options => options.ComparingByMembers<CityDto>().ExcludingMissingMembers());
+
+            createdCity.Id.Should().NotBeEmpty();
+            createdCity.CreatedDate.Should().BeCloseTo(DateTimeOffset.UtcNow, 1000);
+        }
+
+        private City CreateRandomCity() => new()
+        {
+            Id = Guid.NewGuid(),
+            Name = Guid.NewGuid().ToString(),
+            Country = Guid.NewGuid().ToString(),
+            Population = rand.Next(100),
+            Timezone = Guid.NewGuid().ToString(),
+            CreatedDate = DateTimeOffset.UtcNow
+        };
+    }
+}
